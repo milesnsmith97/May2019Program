a8953a06ad21d7e4d9dd587177b3350ffadab20b
Miles61168 <Miles61168@GB-LD1LPW10G035.mastek.com>
2019-07-12 14:22:18 +0100

    Spring Bean Application Object and Test cases created

--- hrapp/src/test/java/com/mastek/training/hrapp/HrAppApplicationTests.java
+++ hrapp/src/test/java/com/mastek/training/hrapp/HrAppApplicationTests.java
@@ -2,15 +2,36 @@ package com.mastek.training.hrapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mastek.training.hrapp.apis.EmployeeService;
+import com.mastek.training.hrapp.entities.Employee;
+
+// Initialise the JUnit Test with Spring Boot Application Environment
+// Spring Boot Test: used to test Spring Application Context
+// with all the test cases identified.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HrAppApplicationTests {
+	
+	//scan in memory all the components and provide the best match object in the component
+	@Autowired
+	EmployeeService empService;
+	
+	@Autowired
+	Employee emp;
+	
+	@Test
+	public void addEmployeeUsingService() {
+		empService.registerEmployee(emp);
+	}
 
 	@Test
 	public void contextLoads() {
+		System.out.println("System Test Executed");
 	}
 
 }
